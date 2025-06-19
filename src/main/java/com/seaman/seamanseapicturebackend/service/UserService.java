680af739689e63f647a34e00b56e6887199d8e18
SeaManzhw <2824848908@qq.com>
2025-06-19 17:11:08 +0800

    生成用户模块基础 1.创建用户表 2.MybatisX生成代码 3.完成用户枚举类编写

---
+++ src/main/java/com/seaman/seamanseapicturebackend/service/UserService.java
@@ -0,0 +1,13 @@
+package com.seaman.seamanseapicturebackend.service;
+
+import com.seaman.seamanseapicturebackend.model.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author SeaManzhw
+* @description 针对表【user(用户)】的数据库操作Service
+* @createDate 2025-06-19 16:46:30
+*/
+public interface UserService extends IService<User> {
+
+}
